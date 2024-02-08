ec24a05e98645e9c4d5eacfdcb4824c294002a9a
Gavbriel015 <av.gabriel015@gmail.com>
2024-02-08 19:00:30 -0400

    Update: added font dependencies

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				mainOrange: '#FF7E1B',
+				
+			}
+		},
+		
 	},
+	
 	plugins: [],
 }
